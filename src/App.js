7497ff20dad7b24962885e3424c678b3ca313e69
Aayush Kumar <ayushk780@gmail.com>
2020-11-02 15:07:00 +0530

    Added button and header components
    Signed-off-by: Aayush Kumar <ayushk780@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {Button} from './components/Button/Button'
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      IRIS-Frontend
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Button id="redbutton" buttonStyle="btn--red-outline" buttonSize='btn--large'>Home</Button> 
+          </Route>
+          <Route path='/ap'>
+            <Button buttonStyle="btn--outline" buttonSize='btn--large'>SIGN UP</Button>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
